a297adf160521b70389ba7e919243856f6510856
tarun <tarunaod5@gecg28.ac.in>
2023-08-16 07:33:26 +0530

    first

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+import tarun from "../assets/tarun.png";
+
+export const data = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/prxwvbTh/Screenshot-195.png",
+    title: "Gym Website",
+    desc: "In this project i have used reactjs to create site components.",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/7hZnymgM/aa55c928ff80f3b2306ba5b501ac4704.jpg",
+    title: "Netlfix Clone",
+    desc: "In this project i have used reactJs to create a netflix clone . For fetching the data i have used TMDB web API . ",
+    link: "",
+    git: "",
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/Y9yFKk88/Screenshot-194.png",
+    title: "Cynthia Clone",
+    desc: "It is an portfolio website clone in which i have used gsap for making various animations. In this project i have made an mouse tracker white dot which follows the mouse pointer and it get skewd when we move in x and y direction . For making the white dot following i have used javascript DOM manipulations.",
+    link: "tarunbeldar.tech",
+    git: "https://github.com/tarunod/cynthiaclone",
+  },
+];
